82bb14e690eb2cc610fee9e19f69fd41c744d1a4
Nadine Agbortarh <Nadinetarh@gmail.com>
2026-06-19 12:21:02 +0200

    Initial Commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Nadine Agbortarh on 19/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HikingSurveyTests/HikingSurveyTests.swift
@@ -0,0 +1,19 @@
+//
+//  HikingSurveyTests.swift
+//  HikingSurveyTests
+//
+//  Created by Nadine Agbortarh on 19/06/2026.
+//
+
+import Testing
+@testable import HikingSurvey
+
+struct HikingSurveyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
