f9f78a515a3fc9e6baad96fc6faa54d1dbc5c22d
Nadine Agbortarh <Nadinetarh@gmail.com>
2026-06-19 15:49:02 +0200

    Return sentiment score from Scorer.score
    Change Scorer.score to return a Double sentiment score instead of void. Add a local sentimentScore variable, set tagger.string, and enumerate paragraph sentiment tags to extract the numeric score and return it. Also add Foundation import so callers can receive the computed sentiment value.

--- HikingSurvey/Scorer.swift
+++ HikingSurvey/Scorer.swift
@@ -4,15 +4,37 @@
 //
 //  Created by Nadine Agbortarh on 19/06/2026.
 //
-
+import Foundation
 import NaturalLanguage
 
 
 class Scorer {
     let tagger = NLTagger(tagSchemes: [.sentimentScore])
     
+    func score(_ text: String) -> Double {
+            var sentimentScore = 0.0
+
+
     func score(_ text: String) {
         tagger.string = text
+        tagger.enumerateTags(
+            in: text.startIndex..<text.endIndex,
+            unit: .paragraph,
+            scheme: .sentimentScore,
+            options: []) { sentimentTag, _ in
+                if let sentimentString = sentimentTag?.rawValue,
+                   let score = Double(sentimentString) {
+                    
+                    sentimentScore = score
+                                       return true
+
+
+                }
+
+
+                return false
+            }
         
+        return sentimentScore
     }
 }
