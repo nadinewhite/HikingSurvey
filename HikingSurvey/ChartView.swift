78eb52f2465f65ea8c8b4ce4505ae414b08cbf01
Nadine Agbortarh <Nadinetarh@gmail.com>
2026-06-22 16:24:57 +0200

    Add ChartView SwiftUI stub
    Add new HikingSurvey/ChartView.swift containing a SwiftUI View stub and preview. This scaffolds a chart UI for the HikingSurvey app; the view body is currently empty and intended for future implementation.

---
+++ HikingSurvey/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  HikingSurvey
+//
+//  Created by Nadine Agbortarh on 22/06/2026.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+       
+    }
+}
+
+#Preview {
+    ChartView()
+}
